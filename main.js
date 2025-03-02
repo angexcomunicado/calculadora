fcbfc08c45e7c33ea102b9244e337719871ba4f7
Angel <angexcomunicado@gmail.com>
2025-03-01 21:32:07 -0700

    Calculadora version con teclas

--- main.js
+++ main.js
@@ -2,73 +2,96 @@ const calculadora = document.getElementById('calculadora');
 const resultado = document.getElementById('result');
 
 let operacion = [];
+let nuevaEntrada = false;
 calculadora.addEventListener('click', agregarNum);
-document.addEventListener('keydown', imprimirTecla);
+document.addEventListener('keydown', funcionTeclas);
 
-function imprimirTecla(e) {
+function funcionTeclas(e) {
     if (e.keyCode >= 48 && e.keyCode <= 57) {
-        console.log(e.key)
+        if (nuevaEntrada) {
+            resultado.value = e.key
+            nuevaEntrada = false;
+        } else {
+            resultado.value += e.key
+        }
     }
+
+    // if (e.keyCode >= 48 && e.keyCode <= 57 && operacion.length > 1) {
+    //     calculadora.reset();
+    //     resultado.value += e.key;
+    // }
+    if (e.key === '-') {restar()};
+    if (e.key === 'Enter') {igual()};
+    if (e.key === '/') {dividir()};
+    if (e.key === 'x') {multiplicar()};
+    if (e.key === '+') {sumar()};
+    if (e.key === 'c' || e.key === 'Escape') {clear()};
+    //console.log(e)
 }
-
-
-
 function agregarNum(e) {
     if (e.target.getAttribute('type') === 'button') {
         if (e.target.className != 'operacion') {
-            resultado.value += e.target.innerText;
+            if (nuevaEntrada) {
+                resultado.value = e.target.innerText;
+                nuevaEntrada = false;
+            } else {
+                resultado.value += e.target.innerText; 
+            }
+            
         }
-        if (e.target.className != 'operacion' && operacion.length > 1) {
-            calculadora.reset();
-            resultado.value += e.target.innerText;
-        }
+    }
+    if (e.target.id === 'clear') {clear()}
+    if (e.target.id === 'dividir') {dividir()}
+    if (e.target.id === 'multiplicar') {multiplicar()}
+    if (e.target.id === 'restar') {restar()}
+    if (e.target.id === 'sumar') {sumar()}
+    if (e.target.id === 'igual') {igual()}
+}
 
-    }
-    if (e.target.id === 'clear') {
-        resultado.value = '';
-        operacion = [];
-    }
-    if (e.target.id === 'dividir') {
-        operacion = [];
-        operacion.push(resultado.value);
-        operacion.push('/');
-        //calculadora.reset(); //Aquí el metodo de reset() al parecer solo funciona cuando es un form, para que se reseteen todos los datos
-        console.log(operacion);
-    }
+function clear() {
+    resultado.value = '';
+    operacion = [];
+}
 
-    if (e.target.id === 'multiplicar') {
-        operacion = [];
+function dividir() {
+    operacion = [];
+    operacion.push(resultado.value);
+    operacion.push('/');
+    nuevaEntrada = true;
+    //calculadora.reset(); //Aquí el metodo de reset() al parecer solo funciona cuando es un form, para que se reseteen todos los datos
+}
+
+function multiplicar() {
+    operacion = [];
+    operacion.push(resultado.value);
+    operacion.push('*');
+    nuevaEntrada = true;
+}
+
+function restar() {
+    operacion = [];
+    operacion.push(resultado.value);
+    operacion.push('-');
+    nuevaEntrada = true;
+}
+
+function sumar() {
+    operacion = [];
+    operacion.push(resultado.value);
+    operacion.push('+');
+    nuevaEntrada = true;
+}
+
+function igual() {
+    if (operacion.length > 2) {
+        operacion[0] = resultado.value;
+        const total2 = eval(operacion.join(''));
+        resultado.value = total2;
+    } else {
         operacion.push(resultado.value);
-        operacion.push('*');
-    }
-    if (e.target.id === 'restar') {
-        operacion = [];
-        operacion.push(resultado.value);
-        operacion.push('-');
-    }
-    if (e.target.id === 'sumar') {
-        operacion = [];
-        operacion.push(resultado.value);
-        operacion.push('+');
-        console.log(operacion)
-        //calculadora.reset();
-    }
-    if (e.target.id === 'igual') {
-        if (operacion.length > 2) {
-            operacion[0] = resultado.value;
-            console.log(operacion);
-            const total2 = eval(operacion.join(''));
-            console.log(total2);
-            resultado.value = total2;
-        } else {
-            console.log(operacion);
-            operacion.push(resultado.value);
-            calculadora.reset()
-            const total = eval(operacion.join('')) //eval() evalua una operacion de string como si fuera código javascript, y .join() lo que está haciendo aquí es contatenar lo que está dentro de la string y quitando los espacios
-            resultado.value = total;
-            console.log(operacion)
-            console.log(total);
-        }
+        calculadora.reset()
+        const total = eval(operacion.join('')) //eval() evalua una operacion de string como si fuera código javascript, y .join() lo que está haciendo aquí es contatenar lo que está dentro de la string y quitando los espacios
+        resultado.value = total;
     }
 }
 
